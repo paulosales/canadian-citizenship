9eb813e7395db42aacf0f81a299488e806aba750
Paulo Rogerio Sales Santos <paulosales@gmail.com>
2026-07-04 17:58:29 -0500

    update the enabled tests to 3

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,5 +1,5 @@
 export const PASS_SCORE = 15;
 export const QUESTIONS_PER_TEST = 20;
 export const TOTAL_TESTS = 60;
-export const ENABLED_TESTS = 4;
+export const ENABLED_TESTS = 3;
 export const TIME_LIMIT_MINUTES = 45;
